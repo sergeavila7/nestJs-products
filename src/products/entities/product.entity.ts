c8b8f8d820ec85f236d9ac18720be49a172ddd02
sergeavila7 <serge_avila7@yahoo.com>
2025-01-17 17:35:33 -0600

    feat(controllers, microservices): implement CRUD methods for product handling

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,20 @@
-export class Product {}
+interface UpdateWithOptions {
+  name?: string;
+  description?: string;
+  price?: number;
+}
+
+export class Product {
+  constructor(
+    public id: string,
+    public name: string,
+    public description: string,
+    public price: number,
+  ) {}
+
+  updateWith({ name, description, price }: UpdateWithOptions) {
+    this.name = name ?? this.name;
+    this.description = description ?? this.description;
+    this.price = price ?? this.price;
+  }
+}
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,26 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-
+import { Product } from './entities/product.entity';
+import { v4 as Uuidv4 } from 'uuid';
 @Injectable()
 export class ProductsService {
+  private products: Product[] = [];
+
   create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+    const { name, description, price } = createProductDto;
+    const newProduct = new Product(Uuidv4(), name, description, price);
+    this.products.push(newProduct);
+
+    return newProduct;
   }
 
   findAll() {
-    return `This action returns all products`;
+    return this.products;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  findOne(id: string): Product {
+    const product = this.products.find((product) => product.id === id);
+
+    if (!product) {
+      throw new NotFoundException(`Product with id ${id} not found`);
+    }
+    return product;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  update(id: string, updateProductDto: UpdateProductDto) {
+    const { id: _, ...values } = updateProductDto;
+
+    const product = this.findOne(id);
+
+    product.updateWith({ ...values });
+    return product;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  remove(id: string) {
+    const product = this.findOne(id);
+    this.products = this.products.filter((product) => product.id !== id);
+    return product;
   }
 }
